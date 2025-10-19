a12ed34127357a20c5a6daa01be07147982a52ce
Aniruddho Roy <aniruddho71@gmail.com>
2025-10-19 20:00:11 +0600

    ignore

--- src/main/java/com/example/project_7/App.java
+++ src/main/java/com/example/project_7/App.java
@@ -2,6 +2,7 @@ package com.example.project_7;
 
 public class App {
     public static void main(String[] args) {
+        System.out.println("Welcome to ByteBook");
         Main.main(args);
     }
 }
