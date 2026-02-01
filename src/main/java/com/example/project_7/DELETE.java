e31975f85f857795c5c633fc968a1ecd5203cd77
Aniruddho Roy <aniruddho71@gmail.com>
2026-02-01 22:07:09 +0600

    nothing

--- src/main/java/com/example/project_7/DELETE.java
+++ src/main/java/com/example/project_7/DELETE.java
@@ -1,14 +1,25 @@
-//package com.example.project_7;
-//
-//public class DELETE {
-//    public static void main(String[] args) {
-//        System.out.println("Radha Madhob");
-//        RecentHandler file = new RecentHandler();
-////        file.add_recent_file(new Item("This is name1","This is path1"));
-//        for(Item item:file.get_recent_files()){
-//            System.out.println(item.name);
-//            System.out.println(item.path);
-//        }
-//
-//    }
-//}
+package com.example.project_7;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import org.fxmisc.richtext.CodeArea;
+
+public class DELETE extends Application {
+    public static void main(String[] args)  {
+    launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        CodeArea codeArea = new CodeArea();
+        codeArea.setWrapText(true);
+        codeArea.autosize();
+        BorderPane root = new BorderPane();
+        root.setCenter(codeArea);
+        Scene scene = new Scene(root,400,400);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
